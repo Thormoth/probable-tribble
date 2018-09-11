bcddc50e8a7f71d7dc5ab02025fb910a63569a63
matt <kudell95@hotmail.com>
2018-09-11 11:11:16 +1000

    Vector

--- task3.cpp
+++ task3.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
 #include "customer.h"
 #include "job.h"
+#include <vector>
 
 using namespace std;
 
 int main()
 {
-  Customer customers[5];
-  customers[0] = Customer("D David", "1 West Street", "123-456");
-  customers[1] = Customer("A Alice", "2 North Street", "234-567");
-  customers[2] = Customer("D David", "1 West Street", "123-456");
-  customers[3] = Customer("D David", "1 West Street", "123-456");
-  customers[4] = Customer("D David", "1 West Street", "123-456");
+  vector<Customer> customers;
+  customers.push_back(Customer("D David", "1 West Street", "123-456"));
+  customers.push_back(Customer("A Alice", "2 North Street", "234-567"));
+  // customers[0] = Customer("D David", "1 West Street", "123-456");
+  // customers[1] = Customer("A Alice", "2 North Street", "234-567");
+  // customers[2] = Customer("D David", "1 West Street", "123-456");
+  // customers[3] = Customer("D David", "1 West Street", "123-456");
+  // customers[4] = Customer("D David", "1 West Street", "123-456");
 
 
   cout << customers[1].get_name() << endl;
