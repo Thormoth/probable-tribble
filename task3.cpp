c9e2074a8957200964d70ce320aa2551c3c1929e
matt <kudell95@hotmail.com>
2018-09-11 18:30:30 +1000

    started work on the job system

--- task3.cpp
+++ task3.cpp
@@ -5,19 +5,35 @@
 
 using namespace std;
 
+Customer findCustomerByName(){
+  
+}
+
 int main()
 {
+
+  int jobs = 0;
+  string cust_name;
+
   vector<Customer> customers;
+  cout << "loading Customers...\n\n";
+
   customers.push_back(Customer("D David", "1 West Street", "123-456"));
   customers.push_back(Customer("A Alice", "2 North Street", "234-567"));
-  // customers[0] = Customer("D David", "1 West Street", "123-456");
-  // customers[1] = Customer("A Alice", "2 North Street", "234-567");
-  // customers[2] = Customer("D David", "1 West Street", "123-456");
-  // customers[3] = Customer("D David", "1 West Street", "123-456");
-  // customers[4] = Customer("D David", "1 West Street", "123-456");
+  customers.push_back(Customer("C Carl", "3 East Street", "345-678"));
+  customers.push_back(Customer("Z Zoro", "2 North Street", "456-789s"));
+
+  cout << "Number of jobs for today: ";
+  cin >> jobs;
+  for (int i = 0; i < jobs; i++) {
+    cout << "Job " << i + 1 << ": \n";
+    cout << "Customer Name: ";
+    cin >> cust_name;
+    /* code */
+  }
 
 
-  cout << customers[1].get_name() << endl;
+  // cout << customers[1].display_all() << endl << customers[3].display_short() << endl;
 
   return 0;
 }
